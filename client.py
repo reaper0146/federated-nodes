63472e47567ada49a5efbb42a329d7ea14359bab
Soumya Pal <pal.soumya1996@gmail.com>
2022-02-09 11:52:27 -0500

    update

--- client.py
+++ client.py
@@ -7,6 +7,8 @@ model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
 # Load CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
 # Define Flower client
 class CifarClient(fl.client.NumPyClient):
@@ -15,13 +17,13 @@ class CifarClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=3, steps_per_epoch=3)
+        model.fit(x_train, y_train, epochs=1, batch_size=2, steps_per_epoch=2)
         return model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
         model.set_weights(parameters)
         loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy": accuracy}
+        return float(loss), len(x_test), {"accuracy": float(accuracy)}
 
 # Start Flower client
 fl.client.start_numpy_client(server_address="[::]:8080", client=CifarClient())
